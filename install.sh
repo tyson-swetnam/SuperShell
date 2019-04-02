0405d2a1c553aa561eef4475428d6e09c327e1d4
Brennan Proudfoot <bwproud@live.unc.edu>
2019-04-02 01:27:06 +0000

    fix install

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+cat consent.txt
+echo
+echo
+while :
+   do
+    echo "Do you consent to this study(y/n): "
+    read -e COMMANDS
+   if [ "$COMMANDS" = "y" ]; then
+       break
+   elif [ "$COMMANDS" = "n" ]; then
+       echo "Ending installation"
+       exit 0
+   else 
+       echo "Unrecognized input. Please use y/n."
+   fi
+done
 sudo apt-get install jq
 cp offline.sh ~/.offline.sh
 echo "bash ~/.offline.sh" >> ~/.bash_profile
