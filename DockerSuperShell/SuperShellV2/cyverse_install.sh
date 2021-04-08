70afcf8cc524cc5b74944430f74a032a914d0fdb
Samuel George <sdgeorge@icloud.com>
2021-04-08 18:24:38 -0400

    Create cyverse_install.sh

---
+++ DockerSuperShell/SuperShellV2/cyverse_install.sh
@@ -0,0 +1,11 @@
+cp offline.sh ~/.offline.sh
+cp interactive.txt ~/.interactive.txt
+cp supershellhelp.txt ~/.supershellinfo.txt
+cp supershellhelp.txt ~/.supershellhelp.txt
+cp supershellhelpmessage.txt ~/.supershellhelpmessage.txt
+cp ruledir.txt ~/.ruledir.txt
+cp add_interactive.sh ~/.add_interactive.sh
+cp push_events.sh ~/.push_events.sh
+cp disable_help.sh ~/.disable_help.sh
+cp enable_help.sh ~/.enable_help.sh
+echo "bash ~/.offline.sh" >> ~/.bash_profile
